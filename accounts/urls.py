89701bfb88fbbf13818e354328f63cf5d0dce859
Eman <emanamberzindani@gmail.com>
2023-01-23 15:55:20 -0600

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from accounts.views import user_signup, user_login, user_logout
 urlpatterns = [
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("signup/", user_signup, name="signup"),
 ]
