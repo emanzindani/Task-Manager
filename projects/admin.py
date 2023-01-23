b238410f6654d8e4f65ddc526e8d18c75c2a26de
Eman <emanamberzindani@gmail.com>
2023-01-23 15:03:07 -0600

    Feature 6 complete

--- projects/admin.py
+++ projects/admin.py
@@ -6,4 +6,8 @@ from projects.models import Project
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "description",
+        "id",
+    )
